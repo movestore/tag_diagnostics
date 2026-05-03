ebbdde64c0c5697ccf58901b221f23886f3b02f0
annescharf <ascharf@ab.mpg.de>
2026-05-04 00:09:34 +0200

    initial code

--- README.md
+++ README.md
@@ -1,77 +1,61 @@
-# Name of App *(Give your app a short and informative title. Please adhere to our convention of Title Case without hyphens (e.g. My New App))*
+# Tag Diagnostics Plots
 
 MoveApps
 
-Github repository: *github.com/yourAccount/Name-of-App* *(provide the link to the repository where the code of the App can be found)*
+Github repository: *https://github.com/movestore/tag_diagnostics.git*
 
 ## Description
-*Enter here the short description of the App that might also be used when filling out the description during App submission to MoveApps. This text is directly presented to Users that look through the list of Apps when compiling Workflows.*
+Creation of plots for diagnostics and monitoring of tags. Number of locations per day, battery voltage and fix rate, and any other attribute can be plotted
 
 ## Documentation
 *Enter here a detailed description of your App. What is it intended to be used for. Which steps of analyses are performed and how. Please be explicit about any detail that is important for use and understanding of the App and its outcomes. You might also refer to the sections below.*
 
 ### Application scope
 #### Generality of App usability
-*State here if the App was developed for a specific species, taxon or taxonomic group, or to answer a specific question. How might it influence the scope and utility of the App. This information will help the user to understand why the App might be producing no or odd results.*
-
-*Examples:*
-
-This App was developed using data of birds. 
-
-This App was developed using data of red deer. 
-
 This App was developed for any taxonomic group. 
 
-This App was developed to identify kill sites, but can probably be used to identify any kind of location clusters like nests, dens or drinking holes.
-
 #### Required data properties
-*State here the required and/or optimal data properties for this App to perform properly.*
-
-*Examples:*
-
-This App is only applicable to data that reflect range resident behavior. 
-
-The data should have a fix rate of at least 1 location per 30 minutes. 
-
 The App should work for any kind of (location) data.
 
 ### Input type
-*Indicate which type of input data the App requires.*
-
-*Example*: `move2::move2_loc`
+`move2::move2_loc`
 
 ### Output type
-*Indicate which type of output data the App produces to be passed on to subsequent Apps.*
-
-*Example:* `move2::move2_loc`
+`move2::move2_loc`
 
 ### Artefacts
-*If the App creates artefacts (e.g. csv, pdf, jpeg, shapefiles, etc), please list them here and describe each.*
-
-*Example:* `rest_overview.csv`: csv-file with Table of all rest site properties
+`tag_diagnostics_plots.pdf`: 
 
 ### Settings 
-*Please list and define all settings/parameters that the App requires to be set by the App user, if necessary including their unit. Please first state the Setting name the user encounters in the Settings menu defined in the appspecs.json, and between brackets the argument used in the R function to be able to identify it quickly in the code if needed.*
 
-*Example:* `Radius of resting site` (radius): Defined radius the animal has to stay in for a given duration of time for it to be considered resting site. Unit: `metres`.
+
+
+
+"Plot number of locations per day" T/F  plot_nb_lcs 
+"Add battery voltage to previous plot" T/F add_vot
+"Column name of battery voltage in the data set" string bat_attr
+"Plot fix rate (approximation)" T/F plot_fix_rate
+"Units to calculate fix rate" dropdown: "sec", "min", "hour","day"  unts_fix_rate
+"Additional attributes to plot as lines" string attr_line
+"Additional attributes to plot as a boxplot per day" string attr_boxplot
+"Use local time to be displayed on the plot" T/F use_local_time
+"Structure of pdf" - "Create pdf grouping plot per track" / "Create pdf grouping plots per attribute" pdfMode = c("perTrack", "perAttrib")
+
+
+
+
+{
+      "id": "attribs",
+      "name": "Data attributes to plot",
+      "description": "Provide the exact names of the data attributes that you want to plot (must be comma-separated! and without quotes). If unsure of attribute names or spelling, please run the first App in your workflow and check the 'event_attributes' in the 'App Output Details' (green 'i'). For definitions of Movebank attributes please refer to the Movebank Attribute Dictionary (https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary).",
+      "defaultValue": null,
+      "type": "STRING"
+	}
 
 ### Changes in output data
-*Specify here how and if the App modifies the input data. Describe clearly what e.g. each additional column means.*
-
-*Examples:*
-
-The App adds to the input data the columns `Max_dist` and `Avg_dist`. They contain the maximum distance to the provided focal location and the average distance to it over all locations. 
-
-The App filterers the input data as selected by the user. 
-
-The output data is the outcome of the model applied to the input data. 
-
 The input data remains unchanged.
 
 ### Most common errors
-*Please describe shortly what most common errors of the App can be, how they occur and best ways of solving them.*
+misspelling attributes
 
 ### Null or error handling
-*Please indicate for each setting as well as the input data which behaviour the App is supposed to show in case of errors or NULL values/input. Please also add notes of possible errors that can happen if settings/parameters are improperly set and any other important information that you find the user should be aware of.*
-
-*Example:* **Setting `radius`:** If no radius AND no duration are given, the input data set is returned with a warning. If no radius is given (NULL), but a duration is defined then a default radius of 1000m = 1km is set. 
--- RFunction.R
+++ RFunction.R
@@ -1,38 +1,247 @@
-library("moveapps")
-library("move2")
-library("lubridate")
+library(move2)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(sf)
+library(lutz)
+library(rlang)
+library(units)
+library(lubridate)
 
-## The parameter "data" is reserved for the data object passed on from the previous app
+# dt <- readRDS("~/Downloads/test__Workflow_Instance_001__Movebank_Location__2026-04-24_22-01-30.rds")
+# data <- filter_track_data(dt, .track_id = c("Lina.ABA63..ebos.9679."))
+# data <- dt
+# names(data)
+# summary(data)
+# 
+# bat_attr <- "eobs_battery_voltage"
+# unts_fix_rate <- "min"
+# use_local_time <- T
+# attr_line <- "eobs_fix_battery_voltage,eobs_speed_accuracy_estimate,eobs_temperature,eobs_used_time_to_get_fix,gpsdop,gpssatellite_count"
+# attr_boxplot <- ""# "eobs_fix_battery_voltage,eobs_speed_accuracy_estimate,eobstemperature,eobs_used_timeto_get_fix,gps_dop,gps_satellite_count"
+# pdfMode <- "perAttrib" #"perTrack" "perAttrib"
+# pdf_file <- "plots_prt.pdf"
+# 
+# 
+# 
+# dtt <- readRDS("~/Downloads/test__Workflow_Instance_002__Movebank_Location__2026-04-26_19-18-22.rds")
+# # data <- filter_track_data(dtt, .track_id = c("Floreana_131_11964"))
+# data <- dtt
+# 
+# names(data)
+# summary(data)
+# 
+# bat_attr <- "tag_voltage"
+# unts_fix_rate <- "min"
+# use_local_time <- T
+# attr_line <- "sigfox_rssi"
+# attr_boxplot <- "sigfox_rssi"
+# pdfMode <- "perAttrib" #"perTrack" "perAttrib"
+# pdf_file <- "plots_per_attribute.pdf"#"plots_per_attribute.pdf" plots_per_track.pdf
 
-# to display messages to the user in the log file of the App in MoveApps
-# one can use the function from the logger.R file:
-# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
 
-# Showcase injecting app setting (parameter `year`)
-rFunction = function(data, sdk, year, ...) {
-  logger.info(paste("Welcome to the", sdk))
-  result <- if (any(lubridate::year(move2::mt_time(data)) == year)) { 
-    data[lubridate::year(move2::mt_time(data)) == year,]
-  } else {
-    NULL
+rFunction <- function(data,
+                      plot_nb_lcs,
+                      add_vot,
+                      bat_attr,
+                      plot_fix_rate,
+                      unts_fix_rate,
+                      attr_line,
+                      attr_boxplot,
+                      use_local_time,
+                      pdfMode = c("perTrack", "perAttrib")
+                      ) {
+  
+  ## check bat attr name
+  batvot_ok <- bat_attr[bat_attr %in% names(data)]
+  ## check line attr name  
+  attr_line_L <- strsplit(attr_line, ",")[[1]]
+  attr_line_L <- gsub(" ", "", attr_line_L, fixed = TRUE)
+  attr_line_ok <- attr_line_L[attr_line_L %in% names(data)]
+  attr_line_error <- attr_line_L[!attr_line_L %in% names(data)]
+  if (length(attr_line_error) > 0) {logger.info(paste0("Warning! Your defined attributes: ",paste0('"',attr_line_error,'"', collapse = ", ")," do not exist in the data set. They will not be plotted."))}
+  ## check boxplot attr name  
+  attr_boxplot_L <- strsplit(attr_boxplot, ",")[[1]]
+  attr_boxplot_L <- gsub(" ", "", attr_boxplot_L, fixed = TRUE)
+  attr_boxplot_ok <- attr_boxplot_L[attr_boxplot_L %in% names(data)]
+  attr_boxplot_error <- attr_boxplot_L[!attr_boxplot_L %in% names(data)]
+  if (length(attr_boxplot_error) > 0) {logger.info(paste0("Warning! Your defined attributes: ",paste0('"',attr_boxplot_error,'"', collapse = ", ")," do not exist in the data set. They will not be plotted."))}
+  
+  
+  data_L <- split(data, mt_track_id(data))
+  
+  # helper to build all plots for one track, return as named list
+  make_track_plots <- function(trk, id) {
+    
+    if (use_local_time) {
+      coords <- st_coordinates(trk)
+      timezns <- tz_lookup_coords(lat = coords[, "Y"], lon = coords[, "X"], method = "accurate")
+      most_frequent_tz <- names(which.max(table(timezns)))
+      trk$timestamp_local <- with_tz(mt_time(trk), tzone = most_frequent_tz)
+      if (length(unique(timezns)) != 1) {logger.warn(paste0("There are multiple local timezones present in the track. ","The most frequent local timezone present is used: ", most_frequent_tz))}
+      mt_time(trk) <- "timestamp_local"
+    }
+    
+    trk$date <- as.Date(mt_time(trk))
+    data_daily <- trk |> count(date, name = "n_fix") |> sf::st_drop_geometry()
+    
+    # if (nrow(data_daily) == 0) {return(NULL)}
+    
+    breaks_noon <- as.POSIXct(paste(seq(min(unique(data_daily$date)), max(unique(data_daily$date)), by = "day"), "12:00:00"), tz = attr(mt_time(trk), "tzone"))
+    if (length(breaks_noon) >= 2) {
+      breaks_noon_subset <- breaks_noon[seq(2, length(breaks_noon), 2)]
+    } else {
+      breaks_noon_subset <- breaks_noon
+    }
+    
+    data_daily <- data_daily |> mutate(date_noon = as.POSIXct(paste(date, "12:00:00"), tz = attr(mt_time(trk), "tzone")))
+    
+    if(plot_nb_lcs){
+    
+    ## nb_volt
+      if(add_vot){
+    if (length(batvot_ok) == 1) {
+      bat_units <- units(trk[[bat_attr]])
+      bat_label <- paste("Battery voltage", ifelse(is.null(bat_units), "", paste0("(", bat_units, ")")))
+      
+      coeff <- max(data_daily$n_fix, na.rm = TRUE) /
+        as.numeric(max(trk[[bat_attr]], na.rm = TRUE))
+      
+      nb_volt <- ggplot() +
+        geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
+        geom_path(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[bat_attr]]) * coeff), colour = "blue") +
+        geom_point(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[bat_attr]]) * coeff), shape = 4, colour = "blue") +
+        scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+        scale_y_continuous(name = "Number GPS fixes per day", sec.axis = sec_axis(~ . / coeff, name = bat_label)) +
+        labs(title = "Number of GPS fixes per day and battery voltage", subtitle = paste("Track: ", id))+
+        theme_bw()
+    } else {
+      nb_volt <- ggplot() +
+        geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
+        scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+        scale_y_continuous(name = "Number GPS fixes per day") +
+        labs(title = "Number of GPS fixes", subtitle = paste("Track: ", id))+
+        theme_bw()
+    }
+      }else{
+        nb_volt <- ggplot() +
+          geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
+          scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+          scale_y_continuous(name = "Number GPS fixes per day") +
+          labs(title = "Number of GPS fixes", subtitle = paste("Track: ", id))+
+          theme_bw() 
+      }
+    }
+    ## fix rate
+    if(plot_fix_rate){
+    fixrt <- ggplot(trk) +
+      geom_boxplot(aes(x = date, y = mt_time_lags(trk, unts_fix_rate), group = date), outliers = FALSE, na.rm = TRUE) +
+      theme_bw() +
+      labs(title = "Fix rate (approx)", subtitle = paste("Track: ", id))+
+      xlab("") +
+      ylab("")
+    }
+    
+    ## other attr lines
+    ggtrk_all_ls <- lapply(seq_along(attr_line_ok), function(i) {
+      atr <- attr_line_ok[i]
+      ggplot(trk) + 
+        geom_line(aes(x = mt_time(trk), y = !!sym(atr)), show.legend = TRUE) +
+        labs(title = atr, subtitle = paste("Track: ", id))+
+        xlab("") +
+        ylab("") +
+        theme_bw()
+    })
+    names(ggtrk_all_ls) <- paste0(attr_line_ok, "_line")
+    
+    ## other attr boxplot
+    ggtrk_all_bx <- lapply(seq_along(attr_boxplot_ok), function(i) {
+      atr <- attr_boxplot_ok[i]
+      ggplot(trk) + 
+        geom_boxplot(aes(x = date, y =!!sym(atr), group = date), outliers = FALSE, na.rm = TRUE) +
+        labs(title = atr, subtitle = paste("Track: ", id))+
+        xlab("") +
+        ylab("") +
+        theme_bw()
+    })
+    names(ggtrk_all_bx) <- paste0(attr_boxplot_ok, "_box")
+    
+    # return as a named list
+    c(list(nb_volt = nb_volt, fixrt   = fixrt), ggtrk_all_ls, ggtrk_all_bx)
   }
-  if (!is.null(result)) {
-    # Showcase creating an app artifact. 
-    # This artifact can be downloaded by the workflow user on Moveapps.
-    artifact <- appArtifactPath("plot.png")
-    logger.info(paste("plotting to artifact:", artifact))
-    png(artifact)
-    plot(result[move2::mt_track_id_column(result)], max.plot=1)
-    dev.off()
-  } else {
-    logger.warn("nothing to plot")
+  
+  # lapply over tracks
+  track_plots_list <- lapply(names(data_L), function(id) {
+    trk <- data_L[[id]]
+    make_track_plots(trk, id)
+  })
+  names(track_plots_list) <- names(data_L)
+  
+  # remove NULL entries for tracks without plots
+  track_plots_list <- Filter(Negate(is.null), track_plots_list)
+  
+  # --- PDF creation logic ---------------------------------------------------
+  if (pdfMode == "perTrack") {
+    
+    # one multi-page PDF; per track, plots are consecutive, 2x3 grid
+    all_pages <- lapply(names(track_plots_list), function(id) {
+      # list of grobs for this track, in fixed order:
+      # nb_volt, fixrt, then attributes
+      grobs_id <- track_plots_list[[id]]
+      # marrangeGrob will paginate if more than 6 for a track [web:18][web:19]
+      marrangeGrob(grobs = grobs_id, nrow = 3, ncol = 2)
+    })
+    
+    # all_pages is a list of "grob lists"; bind them
+    # all_pages <- do.call(c, all_pages)
+    
+    ggsave("tag_diagnostics_plots.pdf", all_pages, width = 10, height = 10)
+    
+  } else if (pdfMode == "perAttrib") {
+    
+    # collect per-attribute (including nb_volt, fixrt) across tracks
+    # 1) named vector of attribute keys per plot list
+    # nb_volt and fixrt are always the first two names
+    # any extra names are attributes
+    # build a long list of plots with tags
+    plot_long <- list()
+    for (id in names(track_plots_list)) {
+      pl <- track_plots_list[[id]]
+      for (nm in names(pl)) {
+        key <- nm              # "nb_volt", "fixrt", or attribute name
+        plot_long[[length(plot_long) + 1]] <-
+          list(key = key, id = id, grob = pl[[nm]])
+      }
+    }
+    
+    # unique keys to iterate over
+    keys <- unique(vapply(plot_long, `[[`, character(1), "key"))
+    
+    # one list element per key; each element is either a list of pages or NULL
+    pages_by_key <- lapply(keys, function(k) {
+      grobs_k <- lapply(plot_long, function(x) {
+        if (x$key == k) x$grob else NULL
+      })
+      grobs_k <- Filter(Negate(is.null), grobs_k)
+      
+      if (length(grobs_k) == 0) {
+        return(NULL)
+      }
+      marrangeGrob(grobs = grobs_k, nrow = 3, ncol = 2)
+    })
+    
+    # drop NULL keys
+    pages_by_key <- Filter(Negate(is.null), pages_by_key)
+    
+    # flatten into a single list of pages (same as your c(all_pages, pages_k) loop)
+    all_pages <- pages_by_key#do.call(c, pages_by_key)
+    
+    if (length(all_pages) > 0) {
+      ggsave("tag_diagnostics_plots.pdf", all_pages, width = 10, height = 10)
+    } else {
+      warning("No plots available for perAttrib pdfMode.")
+    }
   }
-  # Showcase to access a file ('auxiliary files') that is 
-  # a) provided by the app-developer and 
-  # b) can be overridden by the workflow user.
-  fileName <- getAuxiliaryFilePath("auxiliary-file-a")
-  logger.info(readChar(fileName, file.info(fileName)$size))
-
-  # provide my result to the next app in the MoveApps workflow
-  return(result)
+  
+  return(data)
 }

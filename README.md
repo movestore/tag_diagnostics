ef118a240d36e215e86bc0c4e8c6bb6f5713d72e
annescharf <ascharf@ab.mpg.de>
2026-05-04 23:29:45 +0200

    refining

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ Github repository: *https://github.com/movestore/tag_diagnostics.git*
 Creation of plots for diagnostics and monitoring of tags. Number of locations per day, battery voltage and fix rate, and any other attribute can be plotted
 
 ## Documentation
-*Enter here a detailed description of your App. What is it intended to be used for. Which steps of analyses are performed and how. Please be explicit about any detail that is important for use and understanding of the App and its outcomes. You might also refer to the sections below.*
+User has freedom to select what should be plotted. Number of locations per day is calculated from the data, and in the same plot the battery voltage can be added. The fix rate is also calculated from the data (from the time lag between consecutive locations) and represented as boxplots per day. The solid line of the boxplot corresponds to the median value which normally will correspond to the fix rate setting of the tag. Any numeric variable can be plotted as a line over time, and/or summarized as boxplots per day. All boxplots exclude the outliers for better visualization. If a line is plotted all values are included.
+For better interpretation the data can be plotted in local time (timezone of the physical location of the tag).
+
+### Output snapshot
+
+<p align="left">
+  <img src="example_tag_diagnostic.png" width="600">
+</p>
 
 ### Application scope
 #### Generality of App usability
@@ -24,38 +31,32 @@ The App should work for any kind of (location) data.
 `move2::move2_loc`
 
 ### Artefacts
-`tag_diagnostics_plots.pdf`: 
+`tag_diagnostics_plots_by_Attribute.pdf` / `tag_diagnostics_plots_by_Track.pdf`: PDF containing all plots, either grouped by attribute or by track
 
 ### Settings 
 
+`Plot number of locations per day` (plot_nb_lcs): default: true
 
+`To add battery voltage to previous plot choose the attibute containing battery voltage`(bat_attr_prov): dropdown with the most common attribute names for data from Movebank that contain battery voltage values. If needed attribute is not available, use free text field below. Available attibutes: "tag_voltage", "eobs_battery_voltage", "solar_cell_voltage", "battery_charging_voltage", "eobs_fix_battery_voltage", "solar_voltage_percent", "tag_backup_voltage", "tinyfox_sunny_index_start_voltage", "tinyfox_sunny_index_voltage_increase", "voltage_resolution". Default: non is selected
 
+`OR provide column name of battery voltage in the data set to add to the previous plot if not present in the selection above` (bat_attr): Provide the exact name of the column containing the battery voltage. If unsure of attribute names or spelling, please run the first App in your workflow and check the 'event_attributes' in the 'App Output Details' (green 'i'). For definitions of Movebank attributes please refer to the [Movebank Attribute Dictionary](https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary).
 
-"Plot number of locations per day" T/F  plot_nb_lcs 
-"Add battery voltage to previous plot" T/F add_vot
-"Column name of battery voltage in the data set" string bat_attr
-"Plot fix rate (approximation)" T/F plot_fix_rate
-"Units to calculate fix rate" dropdown: "sec", "min", "hour","day"  unts_fix_rate
-"Additional attributes to plot as lines" string attr_line
-"Additional attributes to plot as a boxplot per day" string attr_boxplot
-"Use local time to be displayed on the plot" T/F use_local_time
-"Structure of pdf" - "Create pdf grouping plot per track" / "Create pdf grouping plots per attribute" pdfMode = c("perTrack", "perAttrib")
+`Plot fix rate (approximation)` (plot_fix_rate): Fix rate is calculated from the data by calculating the timelag between consecutive locations. These are summarized per day an visualized as a boxplot. The solid line in the middle of the box is the median fix rate calculated and mostly corresponds to the fix rate programmed on the tag. Outliers are not displayed for better visualization. Default: true
 
+`Units to calculate fix rate` (unts_fix_rate): fix rates can be from seconds to hours or even days. For best possible viasualization the units for the fix rate can be selected. Options: "Seconds", "Minutes", "Hours", "Days". Default: Minutes
 
+`Additional attributes to plot as lines` (attr_line): Provide the exact names of the data attributes that you want to plot as a line (must be comma-separated! and without quotes). If unsure of attribute names or spelling, please run the first App in your workflow and check the 'event_attributes' in the 'App Output Details' (green 'i'). For definitions of Movebank attributes please refer to the [Movebank Attribute Dictionary](https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary)."
 
+`Additional attributes to plot as a boxplot per day` (attr_boxplot): Provide the exact names of the data attributes that you want to plot as boxplot per day (must be comma-separated! and without quotes). If unsure of attribute names or spelling, please run the first App in your workflow and check the 'event_attributes' in the 'App Output Details' (green 'i'). For definitions of Movebank attributes please refer to the [Movebank Attribute Dictionary](https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary).
 
-{
-      "id": "attribs",
-      "name": "Data attributes to plot",
-      "description": "Provide the exact names of the data attributes that you want to plot (must be comma-separated! and without quotes). If unsure of attribute names or spelling, please run the first App in your workflow and check the 'event_attributes' in the 'App Output Details' (green 'i'). For definitions of Movebank attributes please refer to the Movebank Attribute Dictionary (https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary).",
-      "defaultValue": null,
-      "type": "STRING"
-	}
+`Use local time to be displayed on the plot` (use_local_time): sometimes for better interpretation the data can be plotted in local time (timezone of the physical location of the tag). Default: true
+
+`Grouping of plots in PDF`(pdfMode): options: "Group plots per track" or "Group plots per attribute"
 
 ### Changes in output data
 The input data remains unchanged.
 
 ### Most common errors
-misspelling attributes
+If provided attributes are not plotted, either they are empty or they are misspelled. To check the spelling, please go to the first App of the Workflow and check the 'event_attributes' in the 'App Output Details' (green 'i').
 
 ### Null or error handling
--- RFunction.R
+++ RFunction.R
@@ -14,13 +14,14 @@ library(lubridate)
 # names(data)
 # summary(data)
 # 
-# bat_attr <- "eobs_battery_voltage"
+# bat_attr_prov <- "eobs_battery_voltage"
+# bat_attr <- NULL
 # unts_fix_rate <- "min"
 # use_local_time <- T
 # attr_line <- "eobs_fix_battery_voltage,eobs_speed_accuracy_estimate,eobs_temperature,eobs_used_time_to_get_fix,gpsdop,gpssatellite_count"
 # attr_boxplot <- ""# "eobs_fix_battery_voltage,eobs_speed_accuracy_estimate,eobstemperature,eobs_used_timeto_get_fix,gps_dop,gps_satellite_count"
 # pdfMode <- "perAttrib" #"perTrack" "perAttrib"
-# pdf_file <- "plots_prt.pdf"
+# # pdf_file <- "plots_prt.pdf"
 # 
 # 
 # 
@@ -31,7 +32,10 @@ library(lubridate)
 # names(data)
 # summary(data)
 # 
-# bat_attr <- "tag_voltage"
+# plot_nb_lcs <- T
+# bat_attr_prov <- "no_selection"
+# plot_fix_rate <- T
+# bat_attr <- NULL#"eobs_battery_voltage"
 # unts_fix_rate <- "min"
 # use_local_time <- T
 # attr_line <- "sigfox_rssi"
@@ -39,30 +43,29 @@ library(lubridate)
 # pdfMode <- "perAttrib" #"perTrack" "perAttrib"
 # pdf_file <- "plots_per_attribute.pdf"#"plots_per_attribute.pdf" plots_per_track.pdf
 
-voc <- movebank_get_vocabulary()
-names(voc[grep("voltage",voc)])
-
-"tag_voltage"
-"eobs_battery_voltage"
-"solar_cell_voltage"  
-
-"battery_charging_voltage"
-"eobs_fix_battery_voltage"
-"solar_voltage_percent"
-"tag_backup_voltage"
-"tinyfox_sunny_index_start_voltage"
-"tinyfox_sunny_index_voltage_increase"
-"voltage_resolution"
-
-
-names(voc[grep("mV",voc)])
+# voc <- movebank_get_vocabulary()
+# names(voc[grep("voltage",voc)])
+# 
+# "tag_voltage"
+# "eobs_battery_voltage"
+# "solar_cell_voltage"  
+# 
+# "battery_charging_voltage"
+# "eobs_fix_battery_voltage"
+# "solar_voltage_percent"
+# "tag_backup_voltage"
+# "tinyfox_sunny_index_start_voltage"
+# "tinyfox_sunny_index_voltage_increase"
+# "voltage_resolution"
+# 
+# 
+# names(voc[grep("mV",voc)])
 
 
 
 rFunction <- function(data,
                       plot_nb_lcs,
-                      add_vot,
-                     # bat_attr_prov,
+                      bat_attr_prov,
                       bat_attr,
                       plot_fix_rate,
                       unts_fix_rate,
@@ -73,7 +76,20 @@ rFunction <- function(data,
                       ) {
   
   ## check bat attr name
-  batvot_ok <- bat_attr[bat_attr %in% names(data)]
+  if(bat_attr_prov!="no_selection"){
+  batvot_ok <- bat_attr_prov[bat_attr_prov %in% names(data)]
+  batvot_error <- bat_attr_prov[!bat_attr_prov %in% names(data)]
+  if (length(batvot_error) > 0) {logger.info(paste0("Warning! Your selected voltage attribute: ",'"',batvot_error,'"'," does not exist in the data set. It will not be plotted."))}
+  }
+  if(bat_attr_prov == "no_selection" & !is.null(bat_attr)){
+    batvot_ok <- bat_attr[bat_attr %in% names(data)]  
+    batvot_error <- bat_attr[!bat_attr %in% names(data)]
+    if (length(batvot_error) > 0) {logger.info(paste0("Warning! Your provided voltage attribute: ",'"',batvot_error,'"'," does not exist in the data set. It will not be plotted."))}
+  }
+  if(bat_attr_prov == "no_selection" & is.null(bat_attr)){
+    batvot_ok <- NULL
+  }
+  
   ## check line attr name  
   if(!is.null(attr_line)){
   attr_line_L <- strsplit(attr_line, ",")[[1]]
@@ -108,52 +124,62 @@ rFunction <- function(data,
     trk$date <- as.Date(mt_time(trk))
     data_daily <- trk |> count(date, name = "n_fix") |> sf::st_drop_geometry()
     
-    # if (nrow(data_daily) == 0) {return(NULL)}
-    
-    breaks_noon <- as.POSIXct(paste(seq(min(unique(data_daily$date)), max(unique(data_daily$date)), by = "day"), "12:00:00"), tz = attr(mt_time(trk), "tzone"))
-    if (length(breaks_noon) >= 2) {
-      breaks_noon_subset <- breaks_noon[seq(2, length(breaks_noon), 2)]
-    } else {
-      breaks_noon_subset <- breaks_noon
-    }
-    
-    data_daily <- data_daily |> mutate(date_noon = as.POSIXct(paste(date, "12:00:00"), tz = attr(mt_time(trk), "tzone")))
+### removed for now -- alignment of middle of bar with noon
+    # breaks_noon <- as.POSIXct(paste(seq(min(unique(data_daily$date)), max(unique(data_daily$date)), by = "day"), "12:00:00"), tz = attr(mt_time(trk), "tzone"))
+    # if (length(breaks_noon) >= 2) {
+    #   breaks_noon_subset <- breaks_noon[seq(2, length(breaks_noon), 2)]
+    # } else {
+    #   breaks_noon_subset <- breaks_noon
+    # }
+    # 
+    # data_daily <- data_daily |> mutate(date_noon = as.POSIXct(paste(date, "12:00:00"), tz = attr(mt_time(trk), "tzone")))
     
     if(plot_nb_lcs){
     
     ## nb_volt
-      if(add_vot){
+      if (length(batvot_ok) == 1) {
+        if(length(trk[[batvot_ok]][is.na(trk[[batvot_ok]])])==length(trk[[batvot_ok]]))
+          batvot_ok <- NULL
+      }
     if (length(batvot_ok) == 1) {
-      bat_units <- units(trk[[bat_attr]])
+      bat_units <- units(trk[[batvot_ok]])
       bat_label <- paste("Battery voltage", ifelse(is.null(bat_units), "", paste0("(", bat_units, ")")))
       
       coeff <- max(data_daily$n_fix, na.rm = TRUE) /
-        as.numeric(max(trk[[bat_attr]], na.rm = TRUE))
+        as.numeric(max(trk[[batvot_ok]], na.rm = TRUE))
       
-      nb_volt <- ggplot() +
-        geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
-        geom_path(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[bat_attr]]) * coeff), colour = "blue") +
-        geom_point(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[bat_attr]]) * coeff), shape = 4, colour = "blue") +
-        scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+      # nb_volt <- ggplot() +
+      #   # geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
+      #   geom_bar(data = trk, aes(x = date), fill = "grey70") +
+      #   geom_path(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[batvot_ok]]) * coeff), colour = "blue") +
+      #   geom_point(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[batvot_ok]]) * coeff), shape = 4, colour = "blue") +
+      #   # scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+      #   scale_y_continuous(name = "Number GPS fixes per day", sec.axis = sec_axis(~ . / coeff, name = bat_label)) +
+      #   labs(title = "Number of GPS fixes per day and battery voltage", subtitle = paste("Track: ", id))+
+      #   theme_bw()+
+      #   xlab("")
+      
+      nb_volt <- ggplot() +  
+        geom_point(data = trk, aes(x = mt_time(trk), y = as.numeric(.data[[batvot_ok]]) * coeff), shape = 20, size=0, colour = "white") + ## workaround to get both plotted on the same xaxis
+        geom_bar(data = trk, aes(x = date), fill = "grey70") +
+        geom_path(data = trk, aes(x = mt_time(trk)-hours(12), y = as.numeric(.data[[batvot_ok]]) * coeff), colour = "blue") +
+        geom_point(data = trk, aes(x = mt_time(trk)-hours(12), y = as.numeric(.data[[batvot_ok]]) * coeff), shape = 4, colour = "blue") +
         scale_y_continuous(name = "Number GPS fixes per day", sec.axis = sec_axis(~ . / coeff, name = bat_label)) +
-        labs(title = "Number of GPS fixes per day and battery voltage", subtitle = paste("Track: ", id))+
-        theme_bw()
+        labs(title = paste0("Number of GPS fixes per day and ",batvot_ok), subtitle = paste("Track: ", id))+
+        theme_bw()+
+        xlab("")
+      
+      
     } else {
       nb_volt <- ggplot() +
-        geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
-        scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
+        # geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
+        geom_bar(data = data_daily, aes(x = date, y = n_fix), stat = "identity", fill = "grey70") +
+        # scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
         scale_y_continuous(name = "Number GPS fixes per day") +
         labs(title = "Number of GPS fixes", subtitle = paste("Track: ", id))+
-        theme_bw()
+        theme_bw()+
+        xlab("")
     }
-      }else{
-        nb_volt <- ggplot() +
-          geom_bar(data = data_daily, aes(x = date_noon, y = n_fix), stat = "identity", fill = "grey70") +
-          scale_x_datetime(breaks = breaks_noon_subset, name = "", date_labels = "%d %b") +
-          scale_y_continuous(name = "Number GPS fixes per day") +
-          labs(title = "Number of GPS fixes", subtitle = paste("Track: ", id))+
-          theme_bw() 
-      }
     }
     ## fix rate
     if(plot_fix_rate){
@@ -215,13 +241,13 @@ rFunction <- function(data,
       # nb_volt, fixrt, then attributes
       grobs_id <- track_plots_list[[id]]
       # marrangeGrob will paginate if more than 6 for a track [web:18][web:19]
-      marrangeGrob(grobs = grobs_id, nrow = 3, ncol = 2)
+      marrangeGrob(grobs = grobs_id, nrow = 2, ncol = 3)
     })
     
     # all_pages is a list of "grob lists"; bind them
     # all_pages <- do.call(c, all_pages)
     
-    ggsave(appArtifactPath("tag_diagnostics_plots.pdf"), all_pages, width = 10, height = 10)
+    ggsave(appArtifactPath("tag_diagnostics_plots_by_Track.pdf"), all_pages, width = 20, height = 10)
     
   } else if (pdfMode == "perAttrib") {
     
@@ -253,7 +279,7 @@ rFunction <- function(data,
       if (length(grobs_k) == 0) {
         return(NULL)
       }
-      marrangeGrob(grobs = grobs_k, nrow = 3, ncol = 2)
+      marrangeGrob(grobs = grobs_k, nrow = 2, ncol = 3)
     })
     
     # drop NULL keys
@@ -263,7 +289,7 @@ rFunction <- function(data,
     all_pages <- pages_by_key#do.call(c, pages_by_key)
     
     if (length(all_pages) > 0) {
-      ggsave(appArtifactPath("tag_diagnostics_plots.pdf"), all_pages, width = 10, height = 10)
+      ggsave(appArtifactPath("tag_diagnostics_plots_by_Attribute.pdf"), all_pages, width = 20, height = 10)
     } else {
       warning("No plots available for perAttrib pdfMode.")
     }
